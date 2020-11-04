21812394cd8da41cb6aa716c146d254f9d750a48
DarkAce65 <tvdood99@gmail.com>
2020-11-03 23:08:13 -0500

    Add image previews on click

--- frontend/src/components/ImageComponent.tsx
+++ frontend/src/components/ImageComponent.tsx
@@ -1,32 +1,45 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
-import { Card } from 'antd';
+import { Card, Image } from 'antd';
+import styled from 'styled-components';
 
 import { API_SERVER } from '../data/ApiConstants';
 import ImageModel from '../models/Image';
 
 const { Meta } = Card;
 
+const ImageCoverContainer = styled.div`
+  overflow: hidden;
+
+  img {
+    height: 30vh;
+    object-fit: cover;
+  }
+`;
+
 interface Props {
   image: ImageModel;
 }
 
 const ImageComponent: FunctionComponent<Props> = ({ image }) => {
+  const [previewVisible, setPreviewVisible] = useState(false);
+
   return (
     <Card
       cover={
-        <div
-          style={{
-            height: '30vh',
-            overflow: 'hidden',
-            backgroundImage: `url(${API_SERVER}${image.href})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-          }}
-        />
+        <ImageCoverContainer>
+          <Image
+            src={`${API_SERVER}${image.href}`}
+            preview={{
+              visible: previewVisible,
+              onVisibleChange: (visible) => setPreviewVisible(visible),
+            }}
+          ></Image>
+        </ImageCoverContainer>
       }
       bordered={false}
       hoverable={true}
+      onClick={() => setPreviewVisible(true)}
     >
       <Meta title={image.title} />
     </Card>
