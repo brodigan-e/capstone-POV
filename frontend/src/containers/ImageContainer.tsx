4a2a36c4953e6e280896feaea93d17eee0075972
DarkAce65 <tvdood99@gmail.com>
2020-11-03 23:33:40 -0500

    Add loading, empty, and error states for image fetch

--- frontend/src/containers/ImageContainer.tsx
+++ frontend/src/containers/ImageContainer.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 
-import { Col, Row } from 'antd';
+import { Col, Empty, Result, Row, Spin } from 'antd';
 
 import ImageComponent from '../components/ImageComponent';
 import { getImages } from '../data/ImagesClient';
@@ -9,6 +9,9 @@ import Image from '../models/Image';
 interface Props {}
 
 interface State {
+  isFetching: boolean;
+  fetchStatus: 'success' | 'error' | null;
+
   images: Array<Image>;
 }
 
@@ -16,7 +19,7 @@ class ImageContainer extends PureComponent<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = { images: [] };
+    this.state = { isFetching: false, fetchStatus: null, images: [] };
 
     this.fetchImages = this.fetchImages.bind(this);
   }
@@ -26,20 +29,50 @@ class ImageContainer extends PureComponent<Props, State> {
   }
 
   fetchImages() {
-    getImages().then((result) => {
-      this.setState({ images: result });
-    });
+    this.setState({ isFetching: true });
+    getImages()
+      .then((result) => {
+        this.setState({
+          isFetching: false,
+          fetchStatus: 'success',
+          images: result,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          isFetching: false,
+          fetchStatus: 'error',
+        });
+      });
   }
 
-  render() {
+  renderImages() {
+    const { fetchStatus, images } = this.state;
+
+    if (fetchStatus === null) {
+      return <div style={{ height: 150 }} />;
+    } else if (fetchStatus === 'error') {
+      return <Result status="error" title="Failed to fetch images" />;
+    } else if (images.length === 0) {
+      return <Empty description="No images uploaded" />;
+    }
+
     return (
       <Row gutter={[24, 24]}>
-        {this.state.images.map((image) => (
+        {images.map((image) => (
           <Col key={image.href} span={8}>
             <ImageComponent image={image} />
           </Col>
         ))}
       </Row>
+    );
+  }
+
+  render() {
+    return (
+      <Spin size="large" spinning={this.state.isFetching} tip="Fetching images...">
+        {this.renderImages()}
+      </Spin>
     );
   }
 }
