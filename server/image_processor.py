c43174239d5fdf6200600ccb2f24a072b01b89e7
Becker Ewing <ewing.j@northeastern.edu>
2020-11-28 13:11:48 -0700

    PR feedback
    - named constants
    - fix bug with redundant comma printed after last rgb array literal

--- server/image_processor.py
+++ server/image_processor.py
@@ -5,6 +5,7 @@ from PIL import Image
 size = 35, 35
 angular_segments_count = 120
 angular_segments = 360 / angular_segments_count
+size_over_2 = int(size[0]/2)
 
 
 def process_image_for_mcu(image_file):
@@ -18,7 +19,7 @@ def process_image_for_mcu(image_file):
     for theta in range(angular_segments_count):
         copied_image = image.copy()
         rotated_image = copied_image.rotate((360/angular_segments_count) * theta)
-        pixels = list(rotated_image.getdata())[(35 * 17):(35 * 17) + 35]
+        pixels = list(rotated_image.getdata())[(size[0] * size_over_2):(size[0] * size_over_2) + size[0]]
         for i in range(len(pixels)):
             output_pixels[i].append(pixels[i])
 
@@ -26,13 +27,18 @@ def process_image_for_mcu(image_file):
 
 
 def print_pixels_as_mcu_code(rgb_data):
-    print('.ledValues = {\n')
+    # Construct a C-style array initialization line for each row of the pixels
+    crgb_array_literal_rows = list()
     for row in rgb_data:
-        crgbSrings = list(map(lambda pixel: f"CRGB({pixel[0]},{pixel[1]},{pixel[2]})", row))
-        print(f"{{{','.join(crgbSrings)}}},")
+        crgbSrings = list(map(lambda pixel: f'CRGB({pixel[0]},{pixel[1]},{pixel[2]})', row))
+        crgb_array_literal_rows.append('{' + ','.join(crgbSrings) + '}')
+
+    # Print the C-struct initialization statement and join all the array initialization literals together
+    print('.ledValues = {')
+    print(',\n'.join(crgb_array_literal_rows))
     print('}')
 
 
 if __name__ == '__main__':
     the_pixels = process_image_for_mcu(sys.argv[1])
-    print_pixels_as_mcu_code(thePixels)
+    print_pixels_as_mcu_code(the_pixels)
